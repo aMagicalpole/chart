8eb877dc86f18e2d35a2eda87cdc280c7001c91a
aMagicalpole <862843658@qq.com>
2020-06-05 14:12:51 +0800

    新增表格表头配置

--- src/components/functional/default/ITable/ITableComp.vue
+++ src/components/functional/default/ITable/ITableComp.vue
@@ -1,133 +1,172 @@
 <template>
-  <div class="" :style="`--stripeColor:${strip}`">
-    <Table ref="table" stripe  :size="'small'" :columns="columnsData" :data="tableData" :row-class-name="ttt"></Table>
+  <div class :style="`--stripeColor:${stripColor}`">
+    <Table
+      ref="table"
+      stripe
+      :size="'small'"
+      :columns="columnsData"
+      :data="tableData"
+      :row-class-name="ttt"
+    ></Table>
   </div>
 </template>
 
 <script>
-  import FuncCompMixin from '@/mixin/FuncCompMixin'
+import FuncCompMixin from "@/mixin/FuncCompMixin";
+import PnDesigner from "@/utils/PnDesigner";
 
-  export default {
-    name: 'ITableComp',
-    mixins: [FuncCompMixin],
-    attr: {
-      configDataTemp: {
-        headerBackgroundColor: '',
-        headerColor: '',
+export default {
+  name: "ITableComp",
+  mixins: [FuncCompMixin],
+  attr: {
+    configDataTemp: Object.assign(
+      {
+        headerBackgroundColor: "",
+        headerColor: "",
         tableConfig: {
           columns: [
             {
-              type: 'selection',
+              type: "selection",
               width: 60,
-              align: 'center'
+              align: "center"
             },
             {
-              title: 'Name',
-              key: 'name'
+              title: "Name",
+              key: "name"
             },
             {
-              title: 'Sex',
-              key: 'sex'
+              title: "Sex",
+              key: "sex"
             }
           ]
         },
-        customJsCode: '',
-      }
-    },
-    data() {
-      return {
-        columnsData: [
-
-        ],
-        tableData: [
+        customJsCode: "",
+        strip: ""
+      },
+      PnDesigner.buildFuncCompDatasourceField({
+        // 如果组件需要配置数据源，则需要调用此函数构建数据源字段对象
+        ds_resultObjTemplate: [
           {
-            name: 'John Brown',
-            age: 18,
-            address: 'New York No. 1 Lake Park',
-            date: '2016-10-03'
-          },
-          {
-            name: 'Jim Green',
-            age: 24,
-            address: 'London No. 1 Lake Park',
-            date: '2016-10-01'
-          },
-          {
-            name: 'Joe Black',
-            age: 30,
-            address: 'Sydney No. 1 Lake Park',
-            date: '2016-10-02'
-          },
-          {
-            name: 'Jon Snow',
-            age: 26,
-            address: 'Ottawa No. 2 Lake Park',
-            date: '2016-10-04'
+            field: "value",
+            remark: "值"
           }
         ],
-      }
-    },
-    created(){ },
-    mounted() {
-      let _this = this;
-      this.setHeaderStyle()
-      console.log(this.component);
+        ds_resultObj: [
+          {
+            value: 60
+          }
+        ]
+      }),
+      PnDesigner.buildFuncCompGlobalField()
+    )
+  },
+  data() {
+    return {
+      columnsData: [],
+      tableData: [
+        {
+          name: "John Brown",
+          age: 18,
+          address: "New York No. 1 Lake Park",
+          date: "2016-10-03"
+        },
+        {
+          name: "Jim Green",
+          age: 24,
+          address: "London No. 1 Lake Park",
+          date: "2016-10-01"
+        },
+        {
+          name: "Joe Black",
+          age: 30,
+          address: "Sydney No. 1 Lake Park",
+          date: "2016-10-02"
+        },
+        {
+          name: "Jon Snow",
+          age: 26,
+          address: "Ottawa No. 2 Lake Park",
+          date: "2016-10-04"
+        }
+      ],
+      stripColor: ""
+    };
+  },
+  created() {},
+  mounted() {
+    let _this = this;
+    this.setHeaderStyle();
+    this.setStripColor();
+    this.setColumn()
   
-      let newTableConfig = Object.assign({}, this.component.compConfigData.tableConfig);
+    eval(this.component.compConfigData.customJsCode);
+  },
+  methods: {
+    ttt(row, index) {
+      //console.log(row);
+    },
 
-      newTableConfig.columns.map(data=>{
+    // 重置头部样式
+    setHeaderStyle() {
+      console.log(this.component.compConfigData.headerBackgroundColor);
+
+      $(this.$refs.table.$refs.header)
+        .find("th")
+        .css({
+          "background-color": this.component.compConfigData
+            .headerBackgroundColor,
+          color: this.component.compConfigData.headerColor
+        });
+    },
+
+    // 重置表格表头
+    setColumn() {
+      this.columnsData = []
+      let newTableConfig = Object.assign(
+        {},
+        this.component.compConfigData.tableConfig
+      );
+
+      newTableConfig.columns.map(data => {
         if (data.type) {
           this.columnsData.push({
             type: data.type,
             width: 60
-          })
-        }else {
+          });
+        } else {
           this.columnsData.push({
             title: data.title,
             key: data.key
-          })
+          });
         }
-
       });
-
-      eval(this.component.compConfigData.customJsCode)
-
     },
-    methods: {
-      ttt (row, index) {
-        //console.log(row);
-      },
 
-      setHeaderStyle () {
-        console.log(this.component.compConfigData.headerBackgroundColor);
-        
-        $(this.$refs.table.$refs.header).find('th').css({
-          'background-color': this.component.compConfigData.headerBackgroundColor,
-          'color': this.component.compConfigData.headerColor
-        })
-    
-      },
+// 重置斑马纹样式
+    setStripColor() {
+      this.stripColor = this.component.compConfigData.strip;
+    }
+  },
+  computed: {  },
+  watch: {
+    "component.compConfigData.headerBackgroundColor": {
+      handler: "setHeaderStyle",
+      deep: true
     },
-    computed: {
-     strip(){
-       return this.component.compConfigData.headerBackgroundColor
-     }
+    "component.compConfigData.tableConfig.columns": {
+      handler: "setColumn",
+      deep: true
     },
-    watch: {
-      'component.compConfigData.headerBackgroundColor': {
-        handler: 'setHeaderStyle',
-        deep: true
-      },
-      'component.compConfigData.headerColor': {
-        handler: 'setHeaderStyle',
-        deep: true
-      },
+    "component.compConfigData.strip": {
+      handler: "setStripColor",
+      deep: true
     }
   }
+};
 </script>
 
 <style scoped>
-  /*>>> .ivu-table td {
+/*>>> .ivu-table td {
     background-color: #182328;
     color: #fff;
   }
@@ -136,7 +175,11 @@
     font-weight: bold;
     border: none;
   }*/
- >>> .ivu-table-stripe .ivu-table-body tr:nth-child(2n) td, .ivu-table-stripe .ivu-table-fixed-body tr:nth-child(2n) td{
-   background-color: var(--stripeColor);
-  }
+>>> .ivu-table-stripe .ivu-table-body tr:nth-child(2n) td,
+.ivu-table-stripe .ivu-table-fixed-body tr:nth-child(2n) td {
+  background-color: var(--stripeColor);
+}
+>>> .ivu-table th {
+  background-color: var(--headerBackgroundColor);
+}
 </style>
--- src/components/functional/default/ITable/ITableCompForm.vue
+++ src/components/functional/default/ITable/ITableCompForm.vue
@@ -1,76 +1,122 @@
 <template>
-  <div class="">
-
-    <Form :label-width="105">
-
-      <FormItem label="表头背景色">
-        <ColorPicker size="small" v-model="headerBackgroundColor" alpha recommend/>
-      </FormItem>
-      <FormItem label="表头字体颜色">
-        <ColorPicker size="small" v-model="headerColor" alpha recommend/>
-      </FormItem>
-      <FormItem label="">
-        <Button size="small" type="primary" @click="customJsCodeModalVisible = !customJsCodeModalVisible">自定义代码</Button>
-      </FormItem>
-      <!-- <FormItem label="显示斑马纹">
+  <div class>
+    <Tabs size="small">
+      <TabPane label="基础配置" :style="{paddingTop: '10px'}">
+        <Form :label-width="105">
+          <FormItem label="表头背景色">
+            <ColorPicker size="small" v-model="headerBackgroundColor" alpha recommend />
+          </FormItem>
+          <FormItem label="表头字体颜色">
+            <ColorPicker size="small" v-model="headerColor" alpha recommend />
+          </FormItem>
+          <FormItem label>
+            <Button
+              size="small"
+              type="primary"
+              @click="customJsCodeModalVisible = !customJsCodeModalVisible"
+            >自定义代码</Button>
+          </FormItem>
+          <FormItem label>
+            <Button
+              size="small"
+              type="primary"
+              @click="customTableColumnVisible = !customTableColumnVisible"
+            >自定义表头</Button>
+          </FormItem>
+          <!-- <FormItem label="显示斑马纹">
               <i-switch v-model="stripe">
                 <span slot="open"></span>
                 <span slot="close"></span>
               </i-switch>
-      </FormItem> -->
-    
+          </FormItem>-->
+          <FormItem label="表头字体颜色">
+            <ColorPicker size="small" v-model="strip" alpha recommend />
+          </FormItem>
+        </Form>
 
-    </Form>
-
-    <Modal
-        v-model="customJsCodeModalVisible"
-        draggable
-        scrollable
-        title="自定义代码编辑"
-        width="650"
-        :mask="true"
-        :z-index="3">
-      <CodeEditor v-model="customJsCode"></CodeEditor>
-      <div slot="footer">
-        <Button type="default" @click="customJsCodeModalVisible = false">关闭</Button>
-      </div>
-    </Modal>
-
+        <Modal
+          v-model="customJsCodeModalVisible"
+          draggable
+          scrollable
+          title="自定义代码编辑"
+          width="650"
+          :mask="true"
+          :z-index="3"
+        >
+          <CodeEditor v-model="customJsCode"></CodeEditor>
+          <div slot="footer">
+            <Button type="default" @click="customJsCodeModalVisible = false">关闭</Button>
+          </div>
+        </Modal>
+        <!-- 表头编辑 -->
+        <Modal
+          v-model="customTableColumnVisible"
+          draggable
+          scrollable
+          title="表头编辑"
+          width="650"
+          :mask="true"
+          :z-index="3"
+        >
+          <CodeEditor v-model="tableColumnData" mode="json" :on-blur="updateResultObj"></CodeEditor>
+          <div slot="footer">
+            <Button type="default" @click="customTableColumnVisible = false">关闭</Button>
+          </div>
+        </Modal>
+      </TabPane>
+      <TabPane label="数据源" :style="{paddingTop: '10px'}">
+        <DatasourceState targetComp="ITableComp"></DatasourceState>
+      </TabPane>
+    </Tabs>
   </div>
 </template>
 
 <script>
-  import FuncCompFormMixin from '@/mixin/FuncCompFormMixin'
+import FuncCompFormMixin from "@/mixin/FuncCompFormMixin";
 
-  import { createHelpers } from 'vuex-map-fields';
+import { createHelpers } from "vuex-map-fields";
 
-  const { mapFields } = createHelpers({
-    getterType: 'designer/getLayoutItem',
-    mutationType: 'designer/updateLayoutItem',
-  });
+const { mapFields } = createHelpers({
+  getterType: "designer/getLayoutItem",
+  mutationType: "designer/updateLayoutItem"
+});
 
-  export default {
-    name: 'ITableCompForm',
-    mixins: [FuncCompFormMixin],
-    data() {
-      return {
-        customJsCodeModalVisible: false
-      }
-    },
-    mounted() { },
-    methods: {},
-    computed: {
-      ...mapFields({
-        headerBackgroundColor: 'component.compConfigData.headerBackgroundColor',
-        headerColor: 'component.compConfigData.headerColor',
-        customJsCode: 'component.compConfigData.customJsCode',
-      })
+export default {
+  name: "ITableCompForm",
+  mixins: [FuncCompFormMixin],
+  data() {
+    return {
+      customJsCodeModalVisible: false,
+      customTableColumnVisible: false,
+      tableColumnData: ""
+    };
+  },
+  mounted() {
+    console.log(this.columns);
+    this.tableColumnData = JSON.stringify(this.columns, null, 2);
+  },
+  methods: {
+    updateResultObj() {
+        this.$store.commit('designer/updateLayoutItem', {
+          path: 'component.compConfigData.tableConfig.columns',
+          value: JSON.parse(this.tableColumnData)
+        });
     }
+  },
+  computed: {
+    ...mapFields({
+      headerBackgroundColor: "component.compConfigData.headerBackgroundColor",
+      headerColor: "component.compConfigData.headerColor",
+      customJsCode: "component.compConfigData.customJsCode",
+      columns: "component.compConfigData.tableConfig.columns",
+      strip: "component.compConfigData.strip"
+    })
   }
+};
 </script>
 
 <style scoped>
-  .ivu-form-item {
-    margin-bottom: 0px;
-  }
+.ivu-form-item {
+  margin-bottom: 0px;
+}
 </style>
